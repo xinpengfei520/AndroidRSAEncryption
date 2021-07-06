c05f9047cfb8f1d1ba62a748e9a8b0785b7b6e93
Xin Vance <xinpengfei520@gmail.com>
2021-07-06 20:30:25 +0800

    add DES test activity

--- app/src/main/java/com/xpf/android/rsa/encryption/activity/DESEncryptionActivity.java
+++ app/src/main/java/com/xpf/android/rsa/encryption/activity/DESEncryptionActivity.java
@@ -1,6 +1,9 @@
 package com.xpf.android.rsa.encryption.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,11 @@ public class DESEncryptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_des_encryption);
+        EditText etInput = findViewById(R.id.etInput);
+        Button btnEncrypt = findViewById(R.id.btnEncrypt);
+        Button btnDecrypt = findViewById(R.id.btnDecrypt);
+        TextView tvContent = findViewById(R.id.tvContent);
+
+        // TODO: 2021/7/6
     }
 }
